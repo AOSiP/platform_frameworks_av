ee90ab094b5e6657bdd98148f504509feba2bc93
Jinjian Wang <e12714@motorola.com>
2017-11-08 21:12:39 -0600

    audio effects: handle error case deadlock
    If error happen, createEffect_l Exit logic will hold mutex,
    which will cause handler.clear() hang since
    disconnectEffectHandle can not get mutex.
    
    Bug: 68982853
    Test: Simulate a error case into Exitlogic.
    
    Change-Id: I9ceda1444b67e82ddc15a6bf3b0e49991cfd39ab

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -1344,15 +1344,17 @@ sp<AudioFlinger::EffectHandle> AudioFlinger::ThreadBase::createEffect_l(
 
 Exit:
     if (lStatus != NO_ERROR && lStatus != ALREADY_EXISTS) {
-        Mutex::Autolock _l(mLock);
-        if (effectCreated) {
-            chain->removeEffect_l(effect);
-        }
-        if (effectRegistered) {
-            AudioSystem::unregisterEffect(effectId);
-        }
-        if (chainCreated) {
-            removeEffectChain_l(chain);
+        {
+            Mutex::Autolock _l(mLock);
+            if (effectCreated) {
+                chain->removeEffect_l(effect);
+            }
+            if (effectRegistered) {
+                AudioSystem::unregisterEffect(effectId);
+            }
+            if (chainCreated) {
+                removeEffectChain_l(chain);
+            }
         }
         handle.clear();
     }
